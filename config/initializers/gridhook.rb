d895e264d87a5f643d4f29fa968f5c6841725a94
thenatefisher <nate.scott.fisher@gmail.com>
2013-06-23 08:34:58 -0700

    replacing email event with a proc to logger

--- config/initializers/gridhook.rb
+++ config/initializers/gridhook.rb
@@ -5,6 +5,6 @@ Gridhook.configure do |config|
   # The path we want to receive events
   config.event_receive_path = '/sendgrid/event'
 
-  config.event_processor = proc {|p| Rails.logger.info p.inspect}
+  config.event_processor = proc {|p| Rails.logger.info p.inspect} 
 
 end
