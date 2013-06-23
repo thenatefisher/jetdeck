0a34f99e456785bfc76680502182b7ddc11fe938
thenatefisher <nate.scott.fisher@gmail.com>
2013-06-22 21:06:15 -0700

    adding gridhook

--- config/initializers/gridhook.rb
+++ config/initializers/gridhook.rb
@@ -1,10 +1,10 @@
 require "#{Rails.root}/lib/gridhook/email_event_processor" 
 
 Gridhook.configure do |config|
-    
+
   # The path we want to receive events
   config.event_receive_path = '/sendgrid/event'
 
-  config.event_processor = EmailEventProcessor.new()
+  config.event_processor = proc {|event| puts event.inspect}
 
 end
--- lib/gridhook/email_event_processor.rb
+++ lib/gridhook/email_event_processor.rb
@@ -1,7 +1,5 @@
 class EmailEventProcessor
   def call(event)
-    # all of your application-specific code here - creating models,
-    # processing reports, etc
     logger = Logger.new(STDOUT)
     logger.error "from sendgrid: #{event.inspect}"
   end
