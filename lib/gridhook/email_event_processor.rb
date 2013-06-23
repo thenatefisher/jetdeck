d104f8a1c6a72cc8a2c59003a2b95562a9077379
thenatefisher <nate.scott.fisher@gmail.com>
2013-06-23 16:41:13 -0700

    adding spring testing; making more spec model tests; integrated sendgrid webhooks that may actually work..

--- lib/gridhook/email_event_processor.rb
+++ lib/gridhook/email_event_processor.rb
@@ -1,25 +1,27 @@
 class EmailEventProcessor
-  def call(event)
+  def call(events)
 
-    AirframeMessage.find(event.airframe_message_id) do |message|
+    events.each do |event|
+        AirframeMessage.find(event.airframe_message_id) do |message|
 
-        case event.event
-        when "processed"; 
-            message.status_date = Time.now
-            message.status = "sent"
-        when "delivered"; 
-            message.status_date = Time.now
-            message.status = "opened"
-        when "bounced"; 
-            message.status_date = Time.now
-            message.status = "bounced"
-        when "dropped"; 
-            message.status_date = Time.now
-            message.status = "failed"
+            case event.event
+                when "processed"
+                    message.status_date = Time.now
+                    message.status = "sent"
+                when "delivered"
+                    message.status_date = Time.now
+                    message.status = "opened"
+                when "bounced"
+                    message.status_date = Time.now
+                    message.status = "bounced"
+                when "dropped"
+                    message.status_date = Time.now
+                    message.status = "failed"
+            end
+            
+            message.save!
+
         end
-        
-        message.save!
-
     end
 
   end
