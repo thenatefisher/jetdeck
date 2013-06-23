40a5c0b49c25c69e5484737b2c942496d762df93
thenatefisher <nate.scott.fisher@gmail.com>
2013-06-22 20:55:35 -0700

    adding gridhook

--- config/initializers/gridhook.rb
+++ config/initializers/gridhook.rb
@@ -1,7 +1,10 @@
- Gridhook.configure do |config|
+require "#{Rails.root}/lib/gridhook/email_event_processor" 
+
+Gridhook.configure do |config|
+    
   # The path we want to receive events
   config.event_receive_path = '/sendgrid/event'
 
-  config.event_processor = EmailEventProcessor.new
+  config.event_processor = EmailEventProcessor.new()
 
 end
