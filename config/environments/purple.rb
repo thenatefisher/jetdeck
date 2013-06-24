36e6320d787adc4a8e7d37164ac8f36182b81b0a
thenatefisher <nate.scott.fisher@gmail.com>
2013-06-23 19:44:59 -0700

    SendGrid webhooks working, tested on localhost as DMZ

--- config/environments/purple.rb
+++ config/environments/purple.rb
@@ -42,7 +42,7 @@ Jetdeck::Application.configure do
   Paperclip.options[:command_path] = "/usr/bin/"
 
   # actually send emails 
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   
--- lib/gridhook/email_event_processor.rb
+++ lib/gridhook/email_event_processor.rb
@@ -1,28 +1,32 @@
 class EmailEventProcessor
-  def call(events)
-    puts "====== IN EMAIL EVENT PROCESSOR ======"
-    events.each do |event|
-        AirframeMessage.find(event.airframe_message_id) do |message|
 
-            case event.event.downcase
-                when "delivered"
-                    message.status_date = Time.now
-                    message.status = "sent"
-                when "open"
-                    message.status_date = Time.now
-                    message.status = "opened"
-                when "bounce"
-                    message.status_date = Time.now
-                    message.status = "bounced"
-                when "dropped"
-                    message.status_date = Time.now
-                    message.status = "failed"
-            end
-            
-            message.save!
+  def call(event)
 
+    return true if event[:airframe_message_id].blank?
+
+    AirframeMessage.find(event[:airframe_message_id].to_i) do |message|
+
+        next if event[:event].blank?
+
+        case event[:event].downcase
+            when "delivered"
+                message.status_date = Time.now
+                message.status = "sent"
+            when "open"
+                message.status_date = Time.now
+                message.status = "opened"
+            when "bounce"
+                message.status_date = Time.now
+                message.status = "bounced"
+            when "dropped"
+                message.status_date = Time.now
+                message.status = "failed"
         end
+        
+        message.save!
+
     end
 
   end
+
 end
