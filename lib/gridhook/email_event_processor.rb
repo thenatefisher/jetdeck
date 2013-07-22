b19db6b04f780c52bec3b0da9e5bba7a0944e8f0
thenatefisher <nate.scott.fisher@gmail.com>
2013-07-21 23:16:25 -0700

    showing detail in message.save for gridhook processor

--- lib/gridhook/email_event_processor.rb
+++ lib/gridhook/email_event_processor.rb
@@ -2,27 +2,27 @@ class EmailEventProcessor
 
   def call(event)
 
+    puts "INPUT EVENT: #{event[:event]}"
+    puts "INPUT MESSAGE_ID: #{event[:airframe_message_id]}"
+
     return true if event[:airframe_message_id].blank?
 
-    AirframeMessage.find(event[:airframe_message_id].to_i) do |message|
+    message = AirframeMessage.find(event[:airframe_message_id].to_i)
 
-      case event[:event].downcase.strip
-      when "delivered"
-        message.status = "sent"
-      when "open"
-        message.status = "opened"
-      when "bounce"
-        message.status = "bounced"
-      when "dropped"
-        message.status = "failed"
-      end
-
-      puts "MESSAGE SAVE: #{message.save}"
-      puts "MESSAGE: #{message.inspect}"
-      puts "EVENT: #{event[:event]}"
-
+    case event[:event].downcase.strip
+    when "delivered"
+      message.status = "sent"
+    when "open"
+      message.status = "opened"
+    when "bounce"
+      message.status = "bounced"
+    when "dropped"
+      message.status = "failed"
     end
 
+    message.save
+    puts "MESSAGE: #{message.inspect}"
+      
   end
 
 end
